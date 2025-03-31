2d28c1c6d5cf4ffe61e051d2a25435384458af16
mzzz-zzm <mzzz-zzm@example.com>
2025-03-31 01:33:38 +0000

    clean up code

--- demo/src/main/java/com/example/demo/SimpleGrpcApplication.java
+++ demo/src/main/java/com/example/demo/SimpleGrpcApplication.java
@@ -19,7 +19,6 @@ public class SimpleGrpcApplication extends GreetServiceImplBase {
     }
     @Override
     public void streamHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        // use stream api to send 'Hello ' + name with number of sent
         for (int i = 0; i < 10; i++) {
             var res = HelloReply.newBuilder().setMessage("Hello " + request.getName() + ": " + i).build();
             responseObserver.onNext(res);
